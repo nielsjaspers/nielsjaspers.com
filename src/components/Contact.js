a81371b58017d1a9bc85632be243f488b1308ee5
Niels Jaspers <naeliss.github@gmail.com>
2025-02-01 15:09:32 +0100

    feat: added twitter & bluesky links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { FaXTwitter, FaBluesky } from "react-icons/fa6";
 import './Contact.css';
 
 function Contact() {
-  return (
-    <section id="contact">
-      <h2>Contact</h2>
-      <div className="contact-icons">
-        <a href="https://github.com/nielsjaspers" target="_blank" rel="noopener noreferrer">
-          <FaGithub />
-        </a>
-        <a href="https://linkedin.com/in/nielsjasperss" target="_blank" rel="noopener noreferrer">
-          <FaLinkedin />
-        </a>
-        <a href="mailto:zakelijk@nielsjaspers.com">
-          <FaEnvelope />
-        </a>
-      </div>
-    </section>
-  );
+    return (
+        <section id="contact">
+            <h2>Contact</h2>
+            <div className="contact-icons">
+                <a href="https://github.com/nielsjaspers" target="_blank" rel="noopener noreferrer">
+                    <FaGithub />
+                </a>
+                <a href="https://linkedin.com/in/nielsjasperss" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin />
+                </a>
+                <a href="https://bsky.app/profile/nielsjaspers.com" target="_blank" rel="noopener noreferrer">
+                    <FaBluesky />
+                </a>
+                <a href="https://x.com/_nielsjaspers" target="_blank" rel="noopener noreferrer">
+                    <FaXTwitter />
+                </a>
+                <a href="mailto:zakelijk@nielsjaspers.com">
+                    <FaEnvelope />
+                </a>
+            </div>
+        </section>
+    );
 }
 
 export default Contact;
