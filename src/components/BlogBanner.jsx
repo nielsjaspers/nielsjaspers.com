fb95ba00b52be31003ba82d9f0daca61267fcbbe
Niels Jaspers <naeliss.github@gmail.com>
2025-02-07 16:38:06 +0100

    feat: changed site color palette

--- src/components/BlogBanner.jsx
+++ src/components/BlogBanner.jsx
@@ -4,14 +4,16 @@ import "./BlogBanner.css";
 
 function BlogBanner({ imgPath, linkText, linkUrl }) {
     return (
-        <div>
-            <img className="blogBannerImg" src={imgPath} alt="Blog Banner" />
+        <>
+            <div className="blogBannerImgContainer">
+                <img className="blogBannerImg" src={imgPath} alt="Blog Banner" />
+            </div>
             <p>
                 <a className="bannerLinkText" href={linkUrl} target="_blank" rel="noopener noreferrer">
                     {linkText}
                 </a>
             </p>
-        </div>
+        </>
     );
 }
 
