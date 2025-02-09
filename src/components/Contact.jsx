ce0fe4bcab96e599cf0b90ed12e3e9498e415ea0
Niels Jaspers <naeliss.github@gmail.com>
2025-02-09 03:46:41 +0100

    removed Twitter from 'contact'

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
-import { FaXTwitter, FaBluesky } from "react-icons/fa6";
+import { FaBluesky } from "react-icons/fa6";
+{/* import { FaXTwitter } from "react-icons/fa6"; */}
 import './Contact.css';
 
 function Contact() {
@@ -11,15 +12,17 @@ function Contact() {
                 <a href="https://github.com/nielsjaspers" target="_blank" rel="noopener noreferrer">
                     <FaGithub />
                 </a>
-                <a href="https://linkedin.com/in/nielsjasperss" target="_blank" rel="noopener noreferrer">
-                    <FaLinkedin />
-                </a>
                 <a href="https://bsky.app/profile/nielsjaspers.com" target="_blank" rel="noopener noreferrer">
                     <FaBluesky />
                 </a>
+                <a href="https://linkedin.com/in/nielsjasperss" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin />
+                </a>
+                {/*
                 <a href="https://x.com/_nielsjaspers" target="_blank" rel="noopener noreferrer">
                     <FaXTwitter />
                 </a>
+                */}
                 <a href="mailto:zakelijk@nielsjaspers.com">
                     <FaEnvelope />
                 </a>
